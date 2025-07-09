3a5b1e9cdb69011d5733ab3ffcfe4206ebd35fc4
Mihir-patel09 <devlopermihirpatel@gmail.com>
2025-07-09 09:32:32 +0530

    vsvsv

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import "./index.css";
+import emailjs from '@emailjs/browser';
+
+// initialize EmailJS as soon as your app boots:
+emailjs.init(process.env.REACT_APP_EMAILJS_USERID);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  
+  <App />
+
 );
